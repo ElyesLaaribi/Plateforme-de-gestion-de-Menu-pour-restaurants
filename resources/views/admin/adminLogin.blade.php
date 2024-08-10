38561841a228d76725872f9fd0f1b4977746983c
ElyesLaaribi <elyeslaaribii@gmail.com>
2024-08-10 17:12:38 +0100

    fixed titel bug

--- resources/views/admin/adminLogin.blade.php
+++ resources/views/admin/adminLogin.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ADMIN PANEL - {{env('APP_NAME')}} - Taste Better</title>
+    <title>ADMIN PANEL - Foodie - Taste Better</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
@@ -12,7 +12,7 @@
 <body>
     <div class="navbar navbar-expend-lg navbar-dark bg-dark">
         <div class="container">
-            <a href="" class="navbar-brand">ADMIN PANEL | {{env("APP_NAME")}}</a>
+            <a href="" class="navbar-brand">ADMIN PANEL | Foodie</a>
 
 
         </div>
